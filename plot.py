f63a5ccfbcb9992af6154382ac57ad05fd409aef
xxpittip <pit.duw@gmail.com>
2018-02-26 00:46:03 +0100

    even fancier plot

--- plot.py
+++ plot.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
 def plotTest(x):
-    plt.plot(x, color="red")
+    plt.plot(x, color="green")
+    plt.xlim(0,len(x)-1)
+    plt.ylim(min(x),max(x))
     plt.show()
